c3c052f476a0024aa86568e1a29ddf775ba5d276
itunuelijah <itunuelijah@gmail.com>
2022-02-23 14:19:51 +0100

    web security configuration

--- src/main/java/com/phoenix/service/security/WebSecurityConfig.java
+++ src/main/java/com/phoenix/service/security/WebSecurityConfig.java
@@ -1,4 +1,27 @@
 package com.phoenix.service.security;
 
-public class WebSecurityConfig {
+
+import org.hibernate.Session;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+
+public class WebSecurityConfig extends WebSecurityConfigurer {
+     WebSecurityConfigurerAdapter{
+
+     }
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        throws  Exception {
+
+            http.csrf().disable()
+                    .authorizeRequests()
+                    .anyRequest()
+                    .authenticated()
+                    .and()
+                    .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
 }
