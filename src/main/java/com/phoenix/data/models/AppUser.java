9bed10b475d8aa8b8d76277764fc3eb6fa07e3e7
itunuelijah <itunuelijah@gmail.com>
2022-02-23 13:05:16 +0100

    App user

--- src/main/java/com/phoenix/data/models/AppUser.java
+++ src/main/java/com/phoenix/data/models/AppUser.java
@@ -5,6 +5,7 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Data
@@ -18,8 +19,21 @@ public class AppUser {
 
     private String lastname;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)// added for spring security(nullable = false)
     private String email;
+
+    //.....added for spring security
+    @Column(nullable = false)
+    private String password;
+    private boolean enabled;
+
+    @ElementCollection  //we use this to say the relationship because Authority is not an entity but similar to one to many relationship.
+    //normally join collumn will be created to manage 1 to many relationship.
+    //with the @ElementCollection, you can save a spring, enum or any other elemtn that is not an entity,.
+    private List<Authority> authorities;// you can be student then or instructor or admin. thats why it is in list.
+
+    //.......
+
     @Column(length = 500)
     private String address;
 
