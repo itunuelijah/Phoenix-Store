0c844d4a38b3c326a985752b2c82d6ed159914e7
itunuelijah <itunuelijah@gmail.com>
2022-03-30 16:25:20 +0100

    controller

---
+++ src/main/java/com/phoenix/web/controllers/AppUserController.java
@@ -0,0 +1,35 @@
+package com.phoenix.web.controllers;
+
+import com.phoenix.data.dto.AppUserDto;
+import com.phoenix.data.dto.AppUserResponseDto;
+import com.phoenix.service.appUser.AppUserService;
+import com.phoenix.web.exceptions.BusinessLogicException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RequestMapping("/api/appUser")
+@RestController
+public class AppUserController {
+
+    @Autowired
+    private AppUserService appUserService;
+
+    @PostMapping()
+    public ResponseEntity<?>register(@RequestBody AppUserDto appUserDto){
+
+        try{
+            AppUserResponseDto responseDto = appUserService.register(appUserDto);
+            return ResponseEntity.ok().body(responseDto);
+        }
+        catch (BusinessLogicException e){
+            return ResponseEntity.badRequest().body(e);
+        }
+    }
+
+}
