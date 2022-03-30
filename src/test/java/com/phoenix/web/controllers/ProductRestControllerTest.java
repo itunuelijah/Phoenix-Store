e506a0dc7e6ed6971aa3a7f06deebc6876005833
itunuelijah <itunuelijah@gmail.com>
2022-03-30 16:24:03 +0100

    test

--- src/test/java/com/phoenix/web/controllers/ProductRestControllerTest.java
+++ src/test/java/com/phoenix/web/controllers/ProductRestControllerTest.java
@@ -1,27 +1,26 @@
 package com.phoenix.web.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.phoenix.data.dto.ProductDto;
+
 import com.phoenix.data.models.Product;
 import com.phoenix.data.repository.ProductRepository;
-import org.assertj.core.api.Assertions;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockMultipartFile;
+
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.ui.Model;
-import org.springframework.web.multipart.MultipartFile;
+
 
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
