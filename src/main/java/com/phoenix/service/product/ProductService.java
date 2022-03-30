f51ef773190cfb3fd0eeb54aa36a5448d1ce75ca
itunuelijah <itunuelijah@gmail.com>
2022-03-30 16:25:46 +0100

    product service

--- src/main/java/com/phoenix/service/product/ProductService.java
+++ src/main/java/com/phoenix/service/product/ProductService.java
@@ -11,7 +11,7 @@ import java.util.List;
 public interface ProductService {
 
     List<Product> getAllProducts() ;
-    Product findProductById(Long productId)throws ProductDoesNotExistException;
+    Product findProductById(Long productId) throws ProductDoesNotExistException;
     Product createProduct(ProductDto productDto) throws BusinessLogicException;
     Product updateProductDetails(Long productId, JsonPatch productPatch) throws BusinessLogicException;
 
