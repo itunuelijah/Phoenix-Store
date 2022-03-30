a8b27cafba24286855a78eda96f9c3afa8cd9843
itunuelijah <itunuelijah@gmail.com>
2022-03-30 16:24:54 +0100

    controller

--- src/main/java/com/phoenix/web/controllers/ProductRestController.java
+++ src/main/java/com/phoenix/web/controllers/ProductRestController.java
@@ -56,9 +56,5 @@ public class ProductRestController {
         } catch (BusinessLogicException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
-
-
     }
-
-
 }
