cda98f4127f3a83aecd5b64e2b970fed0e8a5ea2
itunuelijah <itunuelijah@gmail.com>
2022-03-30 16:28:14 +0100

    security

---
+++ src/main/java/com/phoenix/security/JwtAuthorizationFilter.java
@@ -0,0 +1,56 @@
+package com.phoenix.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
+
+    public JwtAuthorizationFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain chain) throws IOException, ServletException, ServletException {
+
+        String header = request.getHeader("Authorization");
+
+        if (header == null || !header.startsWith("Bearer ")){
+            chain.doFilter(request, response);
+            return;
+        }
+
+        SecurityContextHolder.getContext().setAuthentication(getAuthenticationToken(request));
+    }
+    //    to set an authentication object, we decript
+    private UsernamePasswordAuthenticationToken getAuthenticationToken(HttpServletRequest request){
+
+
+        String token = request.getHeader("Authorization");
+        if (token != null){
+            String email = JWT.require(Algorithm.HMAC256("MyAppSecret@!"))
+                    .build()
+                    .verify(token.replace("Bearer ", ""))
+                    .getSubject();
+
+            if (email != null){
+                return new UsernamePasswordAuthenticationToken(email, null, new ArrayList<>());
+            }
+        }
+
+        return null;
+    }
+}
