8ba51bc233439b0107cdbdfca8d0ced74167b995
itunuelijah <itunuelijah@gmail.com>
2022-03-30 16:30:59 +0100

    dto

---
+++ src/main/java/com/phoenix/data/dto/AppUserResponseDto.java
@@ -0,0 +1,16 @@
+package com.phoenix.data.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class AppUserResponseDto {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String address;
+}
