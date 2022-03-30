fc8a02580f83cb1452262a873f794b034e56e0bd
itunuelijah <itunuelijah@gmail.com>
2022-03-30 16:29:00 +0100

    repository

--- src/main/java/com/phoenix/data/repository/AppUserRepository.java
+++ src/main/java/com/phoenix/data/repository/AppUserRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
 
     Optional<AppUser> findByEmail(String email);
+
+    boolean existsByEmail (String email);
 }
